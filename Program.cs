054e21e10056b2852f2d534e2ac14b663489d01e
popkirilo0-ux <popkirilo0@gmail.com>
2025-09-03 23:07:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskBase Task = new PersonalTask("", "", 0);
+            Console.WriteLine("Введите название:");
+            string Title = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Введите описание:");
+            string Description = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Какой тип задачи?\nW - Рабочая задача\nP - Личная задача");
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.W:
+                    Console.WriteLine("Укажите дедлайны: год, месяц, день");
+                    int Year = int.Parse(Console.ReadLine());
+                    int Month = int.Parse(Console.ReadLine());
+                    int Day = int.Parse(Console.ReadLine());
+                    Task = new WorkTask(Title, Description, new DateTime(Year, Month, Day));
+                    break;
+                case ConsoleKey.P:
+                    Console.WriteLine("Укажите приоритет от 0 до 10");
+                    byte prioritet = byte.Parse(Console.ReadLine());
+                    Task = new PersonalTask(Title, Description, prioritet);
+                    break;
+            }
+            Console.Clear();
+            while (true)
+            {
+                Console.WriteLine($"R - перезаписать элемент или позначить как выполненное\nP - вывести данные о списке\nEnd - завершить прогрмму");
+                ConsoleKey key = Console.ReadKey().Key;
+                Console.Clear();
+                if (key == ConsoleKey.R)
+                {
+                    Console.WriteLine($"C - поменять название\nD - поменять описание\nT - позначить как выполненное");
+                    switch (Console.ReadKey().Key)
+                    { 
+                        case ConsoleKey.C:
+                            Task.IsComplected = true;
+                            break;
+                        case ConsoleKey.D:
+                            Console.WriteLine("Введите новое описание:");
+                            Task.Description = Console.ReadLine();
+                            break;
+                        case ConsoleKey.T:
+                            Console.WriteLine("Введите новое название:");
+                            Task.Title = Console.ReadLine();
+                            break;
+                        case ConsoleKey.Backspace:
+                            break;
+                    }
+                    Console.Clear();
+                }
+                if (key == ConsoleKey.P)
+                {
+                    Task.PrintInfo();
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                if (key == ConsoleKey.End)
+                    break;
+            }
+        }
+    }
+}
---
+++ TaskBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerConsole
+{
+    abstract class TaskBase : IPrintable
+    {
+        public string Title;
+        public string Description;
+        public bool IsComplected = false;
+        public DateTime CreatedAt = DateTime.Now;
+        public abstract void PrintInfo();
+    }
+
+    class WorkTask : TaskBase
+    {
+        public DateTime deadLine;
+        public WorkTask(string title,string description, DateTime deadLine) 
+        {
+            Title = title;
+            this.deadLine = deadLine;
+            Description = description;
+        }
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"Title: {Title}\n Created in: {CreatedAt}\n Description: {Description}\nDeadLine in {deadLine}\n");
+            if (IsComplected)
+                Console.WriteLine("Выполненно!");
+            else
+                Console.WriteLine("Не выполненно");
+            if (deadLine < DateTime.Now)
+                Console.WriteLine("Начальник даст пиздюлей!");
+        }
+    }
+
+    class PersonalTask : TaskBase
+    {
+        public byte prioritet;
+        public PersonalTask(string title, string description, byte prioritet)
+        {
+            Title = title;
+            this.prioritet = prioritet;
+            Description = description;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"Title: {Title}\n Created in: {CreatedAt}\n Description: {Description}\nPrioritet: {prioritet}\n");
+            if (IsComplected)
+                Console.WriteLine("Выполненно!");
+            else
+                Console.WriteLine("Не выполненно");
+        }
+    }
+}
